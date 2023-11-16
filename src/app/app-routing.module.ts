5a7384bec4b71910d7732c68655ac26f38604929
shehroz665 <shehrozmalik665@gmail.com>
2023-11-16 13:08:52 +0500

    routing handled

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',redirectTo:'portfolio',pathMatch:'full'
+  },
+  {
+    path:'portfolio',component:NavbarComponent,
+    children:[
+    ]
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
